eea809496d8dcad07105107f78efdc3c3270cccb
Kazi Mamun <kazimamun716@gmail.com>
2022-07-11 20:18:15 +0600

    finally submit

---
+++ src/components/Home/OurService.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OurService = () => {
+    return (
+        <div className='d-flex flex-column align-items-center'>
+            <p className='text-center'>
+            With decades of experience in buying, selling, and merchandising, we supply restaurants, grocery stores, institutions, wholesalers, the marine industry, and more with a full range of services. We offer conventional, organic, and specialty produce, floral items, a full line of fresh processed solutions for the retail and food service industries.
+            </p>
+            <button className='btn btn-info'>See Services</button>
+        </div>
+    );
+};
+
+export default OurService;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,43 @@
 import { Link } from "react-router-dom";
 import Banner from "../../components/Home/Banner";
+import OurService from "../../components/Home/OurService";
 import VegitableItems from "../../components/Home/VegitableItems";
 import useVegitables from "../../customHooks/useVegitables";
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
-    const [vegetables] = useVegitables();
-    return (
-        <div>
-            <div>
-                <Banner/>
-            </div>
-            <main>
-                <div className="bg-secondary bg-opacity-25 py-2 d-flex justify-content-around">
-                    <h3>Vegetables</h3>
-                    <Link className="text-decoration-none" to='/vegetables'>Show All Vegetables</Link>
-                </div>
-                <VegitableItems vegetables={vegetables?.slice(0,6)}/>
-            </main>
+  const [vegetables] = useVegitables();
+  return (
+    <div>
+      <div>
+        <Banner />
+      </div>
+      <main>
+        <div className="bg-secondary bg-opacity-25 py-2 d-flex justify-content-around">
+          <h3>Vegetables</h3>
+          <Link className="text-decoration-none" to="/vegetables">
+            Show All Vegetables
+          </Link>
         </div>
-    );
+        <VegitableItems vegetables={vegetables?.slice(0, 6)} />
+      </main>
+        <div className="my-3">
+          <h3 className="text-center text-decoration-underline">
+            Our Services
+          </h3>
+          <OurService />
+        </div>
+        <div>
+        <h3 className="text-center text-decoration-underline">
+            Contact Us
+          </h3>
+          <div>
+            <textarea style={{"height": "100px"}} type="text" as='' className="form-control" />
+            <button className="btn btn-success mt-2">Send Your Message</button>
+          </div>
+        </div>
+    </div>
+  );
 };
 
 export default Home;
